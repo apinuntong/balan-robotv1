18bc91e552e67527b066428031d7b50e9bb47bb4
tong <apinun.tong@gmail.com>
2016-08-29 22:17:43 +0700

    ADD

---
+++ V1/code/MDK-ARM/code/startup_stm32f042x6.d
@@ -0,0 +1,1 @@
+code\startup_stm32f042x6.o: startup_stm32f042x6.s
---
+++ V1/code/Src/main.c
@@ -0,0 +1,554 @@
+/**
+  ******************************************************************************
+  * File Name          : main.c
+  * Description        : Main program body
+  ******************************************************************************
+  *
+  * COPYRIGHT(c) 2016 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f0xx_hal.h"
+
+/* USER CODE BEGIN Includes */
+#define calibation_mode           0    			    
+#define stabilize_mode           	1
+#define Function_1_mode           2    			    
+#define Function_2_mode           3
+/* USER CODE END Includes */
+
+/* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c1;
+
+SPI_HandleTypeDef hspi1;
+
+TIM_HandleTypeDef htim1;
+TIM_HandleTypeDef htim3;
+
+/* USER CODE BEGIN PV */
+/* Private variables ---------------------------------------------------------*/
+uint8_t _function1_lock = 0;
+uint8_t _function2_lock = 0;
+float Input_t_t, Output_t;
+uint32_t time;
+uint16_t x = 100;
+uint16_t y1 = 1000;
+uint16_t y2 = 1000;
+uint16_t y_b1 ;
+uint16_t y_b2 ;
+uint8_t conter =0;
+float r_angle, f_angle, output_pid;
+float LOutput, ROutput;
+double butA_motor = 0;
+double motor_1,motor_2;
+float mun_ref_t=0;
+
+////p = 20, i=10;
+float kp_tong= 0.6,ki_tong = 0.8,kd_tong = 0;
+float errSum_t, dErr_t, error_t, lastErr_t;
+float ref_t1=0.0;
+float actual_robot_speed_Old =0,actual_robot_speed=0,angle_adjusted_Old=0,estimated_speed_filtered =0 ;
+static void PWMControl(float set_motor1, float set_motor2);
+static void mypidtong();
+#include "bs_drone_lib.h"
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+void Error_Handler(void);
+static void MX_GPIO_Init(void);
+static void MX_I2C1_Init(void);
+static void MX_TIM1_Init(void);
+static void MX_TIM3_Init(void);
+static void MX_SPI1_Init(void);
+
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+                
+
+/* USER CODE BEGIN PFP */
+/* Private function prototypes -----------------------------------------------*/
+
+/* USER CODE END PFP */
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+int main(void)
+
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration----------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_I2C1_Init();
+  MX_TIM1_Init();
+  MX_TIM3_Init();
+  MX_SPI1_Init();
+
+  /* USER CODE BEGIN 2 */
+	HAL_TIM_PWM_Start(&htim1,TIM_CHANNEL_2);
+	HAL_TIM_PWM_Start(&htim3,TIM_CHANNEL_1);
+	HAL_GPIO_WritePin(GPIOB,LED1_Pin,GPIO_PIN_RESET);
+	TIM1-> PSC = x;
+	TIM3-> PSC = x;
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+		Initial_MPU6050();
+		HAL_Delay(10);
+		HAL_NVIC_EnableIRQ(EXTI0_1_IRQn);
+		HAL_SPI_Receive_IT(&hspi1, (uint8_t*)(spi_rx_data + spi_rx_data_index), 1);
+	HAL_GPIO_WritePin(GPIOB,DIR_A_Pin,GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOB,E_A_Pin,GPIO_PIN_RESET);
+	HAL_Delay(5000);
+	uint32_t time_now = milli();
+	uint32_t watchdog_time_prev = time_now;
+  while (1)
+  {
+		
+		
+		
+  /* USER CODE END WHILE */
+
+  /* USER CODE BEGIN 3 */
+				time_now = milli();
+		
+    if (time_now - watchdog_time_prev >= 100)
+		{
+			watchdog_time_prev = time_now;
+			HAL_GPIO_TogglePin(GPIOA,LED3_Pin);
+		}
+		
+		if (_Sampling_task_do != 0)
+		{
+			conter++;
+			_Sampling_task_do = 0;
+			Read_MPU6050();
+			AHRS(); 
+			actual_robot_speed = (motor_1 + motor_2) / 2; // Positive: forward
+			if(conter>=6){
+			mypidtong();
+				conter =0;
+			}
+			PID_controller();
+			if(abs_user(q_roll)<300){
+				HAL_GPIO_WritePin(GPIOB,E_A_Pin,GPIO_PIN_RESET);
+				HAL_GPIO_WritePin(GPIOB,E_B_Pin,GPIO_PIN_RESET);
+				butA_motor = constrain((butA_motor + Del_roll * period_dt), -500, 500);
+				if(abs_user((float)ch2/4.00f)>1.0f){
+					motor_1 =butA_motor+((float)ch1/3.0f);
+					motor_2 =butA_motor-((float)ch1/3.0f);
+				}else{
+					motor_1 =butA_motor;
+					motor_2 =butA_motor;
+				}
+				motor_1 = constrain(motor_1/100.00f,-5.0f,5.00f);
+				motor_2 = constrain(motor_2/100.00f,-5.0f,5.0f);
+				PWMControl(motor_1,motor_2);
+			}else{
+				HAL_GPIO_WritePin(GPIOB,E_A_Pin,GPIO_PIN_SET);
+				HAL_GPIO_WritePin(GPIOB,E_B_Pin,GPIO_PIN_SET);
+				PWMControl(0.0f,0.0f);
+			}
+						//if(conter>=3){
+				//if(abs_user(ch2)>5){
+					
+	
+				//conter =0;
+			//}
+		}
+	
+  }
+  /* USER CODE END 3 */
+
+}
+
+/** System Clock Configuration
+*/
+void SystemClock_Config(void)
+{
+
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_PeriphCLKInitTypeDef PeriphClkInit;
+
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_HSI48;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
+  RCC_OscInitStruct.HSICalibrationValue = 16;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI48;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_I2C1;
+  PeriphClkInit.I2c1ClockSelection = RCC_I2C1CLKSOURCE_HSI;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+
+  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
+  /* SysTick_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+}
+
+/* I2C1 init function */
+static void MX_I2C1_Init(void)
+{
+
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.Timing = 0x0000020B;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+    /**Configure Analogue filter 
+    */
+  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+}
+
+/* SPI1 init function */
+static void MX_SPI1_Init(void)
+{
+
+  hspi1.Instance = SPI1;
+  hspi1.Init.Mode = SPI_MODE_SLAVE;
+  hspi1.Init.Direction = SPI_DIRECTION_2LINES;
+  hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
+  hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi1.Init.NSS = SPI_NSS_HARD_INPUT;
+  hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi1.Init.CRCPolynomial = 7;
+  hspi1.Init.CRCLength = SPI_CRC_LENGTH_DATASIZE;
+  hspi1.Init.NSSPMode = SPI_NSS_PULSE_DISABLE;
+  if (HAL_SPI_Init(&hspi1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+}
+
+/* TIM1 init function */
+static void MX_TIM1_Init(void)
+{
+
+  TIM_MasterConfigTypeDef sMasterConfig;
+  TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig;
+  TIM_OC_InitTypeDef sConfigOC;
+
+  htim1.Instance = TIM1;
+  htim1.Init.Prescaler = 0;
+  htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim1.Init.Period = 0;
+  htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim1.Init.RepetitionCounter = 0;
+  if (HAL_TIM_PWM_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
+  sBreakDeadTimeConfig.OffStateIDLEMode = TIM_OSSI_DISABLE;
+  sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
+  sBreakDeadTimeConfig.DeadTime = 0;
+  sBreakDeadTimeConfig.BreakState = TIM_BREAK_DISABLE;
+  sBreakDeadTimeConfig.BreakPolarity = TIM_BREAKPOLARITY_HIGH;
+  sBreakDeadTimeConfig.AutomaticOutput = TIM_AUTOMATICOUTPUT_DISABLE;
+  if (HAL_TIMEx_ConfigBreakDeadTime(&htim1, &sBreakDeadTimeConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  sConfigOC.OCIdleState = TIM_OCIDLESTATE_RESET;
+  sConfigOC.OCNIdleState = TIM_OCNIDLESTATE_RESET;
+  if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  HAL_TIM_MspPostInit(&htim1);
+
+}
+
+/* TIM3 init function */
+static void MX_TIM3_Init(void)
+{
+
+  TIM_MasterConfigTypeDef sMasterConfig;
+  TIM_OC_InitTypeDef sConfigOC;
+
+  htim3.Instance = TIM3;
+  htim3.Init.Prescaler = 0;
+  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim3.Init.Period = 0;
+  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  if (HAL_TIM_PWM_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  HAL_TIM_MspPostInit(&htim3);
+
+}
+
+/** Configure pins as 
+        * Analog 
+        * Input 
+        * Output
+        * EVENT_OUT
+        * EXTI
+*/
+static void MX_GPIO_Init(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOF_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin : IMU_INT_Pin */
+  GPIO_InitStruct.Pin = IMU_INT_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(IMU_INT_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : LED3_Pin LED1_Pin DIR_A_Pin E_A_Pin 
+                           DIR_B_Pin E_B_Pin */
+  GPIO_InitStruct.Pin = LED3_Pin|LED1_Pin|DIR_A_Pin|E_A_Pin 
+                          |DIR_B_Pin|E_B_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, LED3_Pin|LED1_Pin|DIR_A_Pin|E_A_Pin 
+                          |DIR_B_Pin|E_B_Pin, GPIO_PIN_RESET);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI0_1_IRQn, 1, 0);
+  HAL_NVIC_EnableIRQ(EXTI0_1_IRQn);
+
+}
+
+/* USER CODE BEGIN 4 */
+
+static void PWMControl(float set_motor1, float set_motor2)
+{    	
+	
+  if (set_motor1 > 0.0f)
+  {
+		y1 = (48000000/x)/(3200.00*set_motor1);
+			if(y2 !=y_b2){
+			TIM3-> CCR1 = y2/2;
+			TIM3-> ARR = y2;
+				if(TIM3-> CNT > TIM3-> ARR){
+					TIM3-> CNT =0;
+				}
+				y_b2 =y2;
+			}
+
+    HAL_GPIO_WritePin(GPIOB,DIR_B_Pin,GPIO_PIN_SET);
+  } else {
+
+      y1 = -(48000000/x)/(3200.00*set_motor1);
+			if(y2 !=y_b2){
+			TIM3-> CCR1 = y2/2;
+			TIM3-> ARR = y2;
+				if(TIM3-> CNT > TIM3-> ARR){
+					TIM3-> CNT =0;
+				}
+				y_b2 =y2;
+			}
+    HAL_GPIO_WritePin(GPIOB,DIR_B_Pin,GPIO_PIN_RESET);
+		}
+	if (set_motor2 > 0.0f)
+  {
+	y2 = (48000000/x)/(3200.00*set_motor2);
+		
+			if(y1 !=y_b1){
+			TIM1-> CCR2 = y1/2;
+			TIM1-> ARR = y1;
+				if(TIM1-> CNT > TIM1-> ARR){
+					TIM1-> CNT =0;
+				}
+				y_b1 =y1;
+			}
+
+    HAL_GPIO_WritePin(GPIOB,DIR_A_Pin,GPIO_PIN_SET);
+  } else {
+
+			y2 = -(48000000/x)/(3200.00*set_motor2);
+			if(y1 !=y_b1){
+			TIM1-> CCR2 = y1/2;
+			TIM1-> ARR = y1;
+				if(TIM1-> CNT > TIM1-> ARR){
+					TIM1-> CNT =0;
+				}
+				y_b1 =y1;
+			}
+
+    HAL_GPIO_WritePin(GPIOB,DIR_A_Pin,GPIO_PIN_RESET);
+		}
+}
+	
+
+float limit(float input, int min_limit, int max_limit)
+{
+  if (input > max_limit)input = max_limit;
+  if (input < min_limit)input = min_limit;
+  return input;
+}
+void mypidtong(){
+	
+	lastErr_t = error_t;
+  error_t = ((float)ch2) - actual_robot_speed;
+  errSum_t = constrain(errSum_t + (error_t * 0.033f), -50, 50);
+
+  Input_t_t = constrain((kp_tong * error_t) + (ki_tong * errSum_t),-200,200);
+
+}
+
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @param  None
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler */
+  /* User can add his own implementation to report the HAL error return state */
+  while(1) 
+  {
+  }
+  /* USER CODE END Error_Handler */ 
+}
+
+#ifdef USE_FULL_ASSERT
+
+/**
+   * @brief Reports the name of the source file and the source line number
+   * where the assert_param error has occurred.
+   * @param file: pointer to the source file name
+   * @param line: assert_param error line source number
+   * @retval None
+   */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+    ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+
+}
+
+#endif
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+*/ 
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
